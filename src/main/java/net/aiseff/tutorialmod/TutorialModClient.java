53ff7399fe67d4d6208ecdfb7362380185e22d03
Aiseff <dlyabsinetolko@gmail.com>
2024-06-08 16:18:15 +0300

    very important comment

---
+++ src/main/java/net/aiseff/tutorialmod/TutorialModClient.java
@@ -0,0 +1,12 @@
+package net.aiseff.tutorialmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+
+//Very important comment
+public class TutorialModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
